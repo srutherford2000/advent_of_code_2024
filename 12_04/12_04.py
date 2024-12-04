a44f927b0c3ff9dd76fe03aa30f237737b943bf3
Sarah Rutherford <sarah.c.rutherford.mil@army.mil>
2024-12-04 19:22:00 +0000

    day 4

--- 12_04/12_04.py
+++ 12_04/12_04.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
-import re
+
+UP_DOWN_LEFT_RIGHT = [(-1,0),(1,0),(0,-1),(0,1)]
+DIAGONAL = [(-1,-1),(-1,1),(1,-1),(1,1)]
 
 def get_2D_arr(file_path):
     arr = []
@@ -12,13 +14,13 @@ def check_match(arr, i, j, mult_i, mult_j):
     return (0<= i+mult_i*3 < len(arr)) and \
            (0<= j+mult_j*3 < len(arr[0])) and \
            (arr[i + mult_i][j + mult_j] == "M") and \
-           (arr[i+ mult_i*2][j + mult_j*2] == "A") and \
-           (arr[i+mult_i*3][j+mult_j*3] == "S")
+           (arr[i + mult_i*2][j + mult_j*2] == "A") and \
+           (arr[i + mult_i*3][j + mult_j*3] == "S")
 
-def find_matches_normal(arr, i, j):
+def find_mas(arr, i, j):
     counts = 0
 
-    potential_dirs = [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(-1,1),(1,-1),(1,1)]
+    potential_dirs = UP_DOWN_LEFT_RIGHT + DIAGONAL
     
     for i_mult, j_mult in potential_dirs:
         if check_match(arr, i, j, i_mult, j_mult):
@@ -27,14 +29,17 @@ def find_matches_normal(arr, i, j):
     return counts
 
 
-def find_matches_x(arr, i, j):
-    if ((j-1) >= 0) and ((j+1) < len(arr[i])) and ((i-1) >= 0) and ((i+1) < len(arr)):
-        vals = [arr[i-1][j-1], arr[i-1][j+1], arr[i+1][j-1], arr[i+1][j+1] ]
+def valid_x(arr, i, j):
+    #the x will need +/- 1 from i and j, so ensure it fits
+    if (0 < j < len(arr[i])-1) and (0 < i < len(arr)-1):
+
+        vals = [arr[i+a][j+b] for a,b in DIAGONAL]
+
         if (2 == vals.count("M")) and (2 == vals.count("S")) and \
            (arr[i-1][j-1]!=arr[i+1][j+1]) and (arr[i-1][j+1]!= arr[i+1][j-1]):
-            return 1
+            return True
 
-    return 0
+    return False
 
 def _12_04_part1(input_data):
     num_xmas_found = 0
@@ -42,7 +47,7 @@ def _12_04_part1(input_data):
     for i, line in enumerate(input_data):
         for j, letter in enumerate(line):
             if "X" == letter:
-                num_xmas_found += find_matches_normal(arr, i, j)
+                num_xmas_found += find_mas(arr, i, j)
     
     return num_xmas_found
 
@@ -52,7 +57,7 @@ def _12_04_part2(input_data):
     for i, line in enumerate(input_data):
         for j, letter in enumerate(line):
             if "A" == letter:
-                num_xmas_found += find_matches_x(arr, i, j)
+                num_xmas_found += 1 if valid_x(arr, i, j) else 0
     
     return num_xmas_found
 
